aae03f8b7eddc8d3703036faa00d0ab792f7ff0f
erlan1405 <erlander5551@gmail.com>
2024-12-11 15:31:13 -0500

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
 import { About } from "./pages/About";
 import { Services } from "./pages/Services";
-import { Navbar }  from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
